c26074a1df45d73b0e32694c98b8ad7b170b7284
Suvixx <sudiptap48@gmail.com>
2021-02-27 18:31:30 +0100

    Update main_fun 2.R

--- main_fun 2.R
+++ main_fun 2.R
@@ -27,3 +27,22 @@ View(pred_prob_n)
 library(caret)
 confusionMatrix(y_test, pred_prob)
 
+#tune for better accuracy <0.9833
+svm_tune <- tune(svm, train.x=train_data$Temperature, train.y=y, 
+                 kernel="radial", ranges=list(cost=10^(-1:3), gamma=c(.5,1,2,3)))
+
+svm_model_after_tune <- svm(train_data$Temperature,y, cost=100, gamma=0.5, probability = TRUE)
+summary(svm_model_after_tune)
+
+pred <- predict(svm_model_after_tune, test_data$Temperature, decision.values = TRUE, probability = TRUE)
+
+confusionMatrix(y_test, pred)
+
+
+fun <- function() {
+  Temp <- readline("Enter a temperature: ")
+  Temp <- as.integer(Temp)
+  predt <- predict(svm_model_after_tune, Temp, decision.values = FALSE, probability = FALSE)
+  cat(as.integer(predt[[1]]) - 1)
+}
+#if(interactive()) fun()
