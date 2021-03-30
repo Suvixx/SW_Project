29da59a3de0d99ba77ae6447e260030104a9569d
Suvixx <sudiptap48@gmail.com>
2021-03-30 23:35:09 +0200

    Random test data generation for integration testing

--- Refactoring/refact.R
+++ Refactoring/refact.R
@@ -56,3 +56,30 @@ FALSE, probability = FALSE)
   
 }
 if(interactive()) fun()
+
+
+
+#random test data generation
+temperature <- data.frame(temp = floor(runif(300, min=-30, max=30)))
+#corresponding label generation
+dress_class <- data.frame(dress_class = rep(0,nrow(temperature)))
+for(i in 1:nrow(temperature))
+{
+  if(temperature[i,1]>=20)
+    dress_class[i,1] = 0
+  if(temperature[i,1] <=19){
+    if(temperature[i,1] >= 10)
+      dress_class[i,1] = 1
+  } 
+  if(temperature[i,1] >=0){
+    if(temperature[i,1] <= 9)
+      dress_class[i,1] = 2
+  }
+  if(temperature[i,1] >=-10){
+    if(temperature[i,1] <= -1)
+      dress_class[i,1] = 3} 
+  if(temperature[i,1]<=-11)
+    dress_class[i,1] = 4
+}
+
+dress_class <- as.factor(dress_class$dress_class)
