f96642feb7c0230728dcccfb36bbe856c6e61ba8
SaloniKwatra0802 <saloni.mcs18.sau@gmail.com>
2021-03-30 22:38:54 +0200

    Refactoring type: Replace code with library use (used built-in function confusionMatrix() from library caret))

--- Refactoring/refact.R
+++ Refactoring/refact.R
@@ -21,22 +21,6 @@ svm_untuned <- svm(train_data$Temperature, y, probability = TRUE)
 pred_prob <- predict(svm_untuned, test_data$Temperature, decision.values
 = TRUE, probability = TRUE)
 
-pred_prob <- as.data.frame(pred_prob)
-y_test <- as.data.frame(y_test)
-#confusion matrix function
-confusion = function(pred_prob,y_test){
-   conf <- matrix(data = rep(0,25), nrow = 5, ncol = 5)
-   rownames(conf) <- c(0,1,2,3,4)
-   colnames(conf) <- c(0,1,2,3,4)
-   for(i in 1:nrow(pred_prob))
-   {
-     if(identical(pred_prob[i,1], y_test[i,1]) == TRUE)
-     {conf[pred_prob[i,1],pred_prob[i,1]] <-
-conf[pred_prob[i,1],pred_prob[i,1]] + 1}
-     else
-       conf[pred_prob[i,1],y_test[i,1]] <-
-conf[pred_prob[i,1],y_test[i,1]] + 1
-   }
-   return(conf)
-}
-confusion_matrix <- confusion(pred_prob = pred_prob, y_test = y_test)
+
+library(caret)
+confusionMatrix(y_test, pred_prob)
